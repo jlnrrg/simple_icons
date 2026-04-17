5e5a86a45ddcaacdb1a785a1593501658e12e9dd
PiscesXD <rclu2881@gmail.com>
2026-04-18 04:22:23 +0800

    fix: flutter 3.41+ compatibility

--- lib/src/icon_data.dart
+++ lib/src/icon_data.dart
@@ -1,11 +1,11 @@
-import 'package:flutter/widgets.dart';
+// import 'package:flutter/widgets.dart';
 
 /// Constructor for [IconData] which includes the font family
-class SimpleIconData extends IconData {
-  const SimpleIconData(int codePoint)
-      : super(
-          codePoint,
-          fontFamily: 'SimpleIcons',
-          fontPackage: 'simple_icons',
-        );
-}
+// class SimpleIconData extends IconData {
+//   const SimpleIconData(int codePoint)
+//       : super(
+//           codePoint,
+//           fontFamily: 'SimpleIcons',
+//           fontPackage: 'simple_icons',
+//         );
+// }
